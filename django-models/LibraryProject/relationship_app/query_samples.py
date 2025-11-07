9e463806e6df67b6179ad4de7860128ed70b88f2
yousseflj9 <youssefmomen@outlook.com>
2025-11-07 16:33:00 +0100

    Initialize Django LibraryProject with CRUD operations for Book model and relationships

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,19 @@
+from .models import Author, Book, Library, Librarian
+
+def sample_queries():
+    # One-to-Many: Get all books by a specific author
+    author = Author.objects.get(name="Robert Green")
+    books_by_author = Book.objects.filter(author=author)
+    
+    # Many-to-Many: Get all books in a specific library
+    library = Library.objects.get(name="Central Library")
+    books_in_library = library.books.all()
+    
+    # One-to-One: Get the librarian of a specific library
+    librarian = Librarian.objects.get(library=library)
+    
+    return {
+        "books_by_author": books_by_author,
+        "books_in_library": books_in_library,
+        "librarian": librarian,
+    }
