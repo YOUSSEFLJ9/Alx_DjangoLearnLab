40ce5231b7afe2086bb3ecef31c75d303a7ce1b6
yousseflj9 <youssefmomen@outlook.com>
2025-11-07 16:40:44 +0100

    Fix library retrieval in sample_queries function for clarity and accuracy

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,12 +6,13 @@ def sample_queries():
     books_by_author = Book.objects.filter(author=author)
     
     # Many-to-Many: Get all books in a specific library
-    library_name = Library.objects.get(name="Central Library")
-    books_in_library = library_name.books.all()
-
+    library_name = "Central Library"
+    library = Library.objects.get(name=library_name)
+    books_in_library = library.books.all()
+    
     # One-to-One: Get the librarian of a specific library
-    librarian = Librarian.objects.get(library=library_name)
-
+    librarian = Librarian.objects.get(library=library)
+    
     return {
         "books_by_author": books_by_author,
         "books_in_library": books_in_library,
