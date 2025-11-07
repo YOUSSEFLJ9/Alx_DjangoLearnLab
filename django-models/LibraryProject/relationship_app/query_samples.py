a7f7794f0e0f13929a774bc3b42e69d279ee2cff
yousseflj9 <youssefmomen@outlook.com>
2025-11-07 16:41:49 +0100

    Update sample_queries function to accept parameters for library and author names

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,12 +1,13 @@
 from .models import Author, Book, Library, Librarian
 
-def sample_queries():
+def sample_queries(library_name, author_name):
     # One-to-Many: Get all books by a specific author
-    author = Author.objects.get(name="Robert Green")
+    # author_name = "Robert Green"
+    author = Author.objects.get(name=author_name)
     books_by_author = Book.objects.filter(author=author)
     
     # Many-to-Many: Get all books in a specific library
-    library_name = "Central Library"
+    # library_name = "Central Library"
     library = Library.objects.get(name=library_name)
     books_in_library = library.books.all()
     
