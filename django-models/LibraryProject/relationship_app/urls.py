3e5afb78eab9ce02211318edc6d285e2870017d6
yousseflj9 <youssefmomen@outlook.com>
2025-12-10 19:14:48 +0100

    fix :work with login/logout insted of auth.views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,18 @@
 from django.urls import include, path
 from . import views
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login
+# from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+# from django.contrib.auth.views import LogoutView
 
 
+#how to use login and logout views
 
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('books/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
     path('register/', views.register, name='register'),
 ]
